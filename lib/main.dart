2c1870cf4128b094b80fa132ec8f5bb72a210d61
Sreeganesh Siva <SR33G33@users.noreply.github.com>
2020-07-24 12:38:48 -0700

    Finished Page transfer for Sign in and Sign Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,99 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("HELP TO SHOP"),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
+    home: Home()
 
-      body: Center(
-        heightFactor: 10,
-        child: Text(
-          "Please create an acount",
-          style: TextStyle(
-            fontSize: 32.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.amber,
-          ) ,
-        ),
-      ),
-
-
-      floatingActionButton: Align(
-        alignment: Alignment.centerRight,
-        child: Row(
-            children: [
-              RaisedButton(
-                child: Text(
-                  'Customer',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.green,
-                  ) ,
-                ),
-                onPressed: (){},
-              ),
-              RaisedButton(
-                child: Text(
-                  'driver',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.green,
-                  ) ,
-                ),
-                onPressed: (){},
-              ),
-              RaisedButton(
-                child: Text(
-                  'store',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.green,
-                  ) ,
-                ),
-                onPressed: (){},
-              ),
-            ]
-        ),
-      ),
-
-    ),
 ));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Help2Shop",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[700],
+      ),
+
+      floatingActionButton: Center(
+        child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              RaisedButton(
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ) ,
+                ),
+                onPressed: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignIn())
+                  );
+                },
+              ),
+
+              RaisedButton(
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ) ,
+                ),
+                onPressed: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignUp())
+                  );
+                },
+              ),
+            ]
+        ),
+      ),
+    );
   }
 }
 
 
+class SignIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign In",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
 
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue[700],
+      ),
+    );
+  }
+}
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign Up",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green[700],
+      ),
+    );
+  }
+}
