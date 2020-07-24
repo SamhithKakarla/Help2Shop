c34039db052e014948f321d27c673a07f764dc74
Sreeganesh Siva <SR33G33@users.noreply.github.com>
2020-07-24 16:48:51 -0700

    Added store class and product class

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class Home extends StatelessWidget {
   }
 }
 
+
 class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -129,14 +130,114 @@ class SignIn extends StatelessWidget {
 class SignedInShopper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Test Shopper Signed In",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[700],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '\n\nPlease Select What Type of User You Are',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FlatButton(
+                child: Text(
+                  'Shopper',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+                color: Colors.red[700],
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignedInShopper()));
+                },
+              ),
+              FlatButton(
+                child: Text(
+                  'Store',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+                color: Colors.red[700],
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => SignedInStore()));
+                },
+              ),
+            ]),
+      ),
+    );
   }
 }
 
-class SignedInStore extends StatelessWidget {
+class SignedInStore extends StatefulWidget {
+  @override
+  SignedInStoreState createState() => SignedInStoreState();
+}
+class SignedInStoreState extends State<SignedInStore> {
+
+
+  static Product water = new Product("water", 1.00);
+  List<Product> productList = [water];
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "safeway",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[700],
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //user input
+          setState(() {
+            Product newp = new Product("whatever the user inputs", 30.0);
+            productList.add(newp);
+          });
+        },
+        child: Text(
+            "add product"
+        ),
+      ),
+      body: Text(
+          "$productList"
+      ),
+    );
   }
 }
 
@@ -205,50 +306,6 @@ class SignUp extends StatelessWidget {
                 },
               ),
             ]),
-      ),
-    );
-  }
-}
-class exampleStore extends StatefulWidget {
-  @override
-  _exampleStoreState createState() => _exampleStoreState();
-}
-
-class _exampleStoreState extends State<exampleStore> {
-
-
-  Product water = new Product("water", 1.00);
-  List<Product> productList = new List(5);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "safeway",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.red[700],
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //user input
-          setState(() {
-            Product newp = new Product("whatever the user inputs", 30.0);
-            productList.add(newp);
-          });
-        },
-        child: Text(
-            "add product"
-        ),
-      ),
-      body: Text(
-          "$productList"
       ),
     );
   }
